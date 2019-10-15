730018b0ee0808b2f50dfeeff95561695ef785b3
Viyzed <Andrew@Andrews-MacBook-Air.local>
2019-10-15 16:29:03 +0100

    This is the first commit for the compiler project for the coursework of Languages and Compilers - CM4106

---
+++ Compiler/Position.cs
@@ -0,0 +1,24 @@
+﻿namespace Compiler
+{
+    /// <summary>
+    /// A position in a file
+    /// </summary>
+    public class Position
+    {
+
+        int lineNo { get; } = 0;
+        int position { get; } = 0;
+
+        public Position(int lineNo, int position)
+        {
+            this.lineNo = lineNo;
+            this.position = position;
+        }
+
+        public override string ToString()
+        {
+            return "The error is located at line " + this.lineNo + " at position " + this.position;
+        }
+
+    }
+}
---
+++ Compiler/Tokenization/Token.cs
@@ -0,0 +1,39 @@
+﻿namespace Compiler.Tokenization
+{
+    /// <summary>
+    /// A token in the source language
+    /// </summary>
+    public class Token
+    {
+        /// <summary>
+        /// The type of the token
+        /// </summary>
+        public TokenType Type { get; }
+
+        /// <summary>
+        /// The actual text associated with the token
+        /// </summary>
+        public string Spelling { get; }
+
+        public Position tokenStartPosition { get; }
+
+        /// <summary>
+        /// Creates a token in the source language
+        /// </summary>
+        /// <param name="type">The type of the token</param>
+        /// <param name="spelling">The spelling of the token</param>
+        public Token(TokenType type, string spelling, Position position)
+        {
+            Spelling = spelling;
+            Type = type;
+            tokenStartPosition = position;
+
+        }
+
+        /// <inheritDoc />
+        public override string ToString()
+        {
+            return $"type={Type}, spelling=\"{Spelling}\", position={tokenStartPosition}";
+        }
+    }
+}
